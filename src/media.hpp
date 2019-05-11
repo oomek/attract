501ba11468fa6e3e57c517507e7acf30fe6eeb6a
Radek Dutkiewicz <radicdotkey@gmail.com>
2019-05-11 02:03:13 +0100

    Moved classes definitions to the header file

--- src/media.hpp
+++ src/media.hpp
@@ -24,8 +24,55 @@
 #define MEDIA_HPP
 
 #include <Audio/SoundStream.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 #include <vector>
 #include <string>
+#include <queue>
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+
+#define FE_HWACCEL (LIBAVUTIL_VERSION_INT >= AV_VERSION_INT( 55, 78, 100 ))
+
+#if FE_HWACCEL
+#include <libavutil/hwcontext.h>
+#endif
+
+#if USE_SWRESAMPLE
+ #include <libswresample/swresample.h>
+ #include <libavutil/opt.h>
+ #define DO_RESAMPLE
+ #define RESAMPLE_LIB_STR " / swresample "
+ #define RESAMPLE_VERSION_MAJOR LIBSWRESAMPLE_VERSION_MAJOR
+ #define RESAMPLE_VERSION_MINOR LIBSWRESAMPLE_VERSION_MINOR
+ #define RESAMPLE_VERSION_MICRO LIBSWRESAMPLE_VERSION_MICRO
+ typedef SwrContext ResampleContext;
+ inline void resample_free( ResampleContext **ctx ) { swr_free( ctx ); }
+ inline ResampleContext *resample_alloc() { return swr_alloc(); }
+ inline int resample_init( ResampleContext *ctx ) { return swr_init( ctx ); }
+#else
+ #if USE_AVRESAMPLE
+  #include <libavresample/avresample.h>
+  #include <libavutil/opt.h>
+  #define DO_RESAMPLE
+  #define RESAMPLE_LIB_STR " / avresample "
+  #define RESAMPLE_VERSION_MAJOR LIBAVRESAMPLE_VERSION_MAJOR
+  #define RESAMPLE_VERSION_MINOR LIBAVRESAMPLE_VERSION_MINOR
+  #define RESAMPLE_VERSION_MICRO LIBAVRESAMPLE_VERSION_MICRO
+  typedef AVAudioResampleContext ResampleContext;
+  inline void resample_free( ResampleContext **ctx ) { avresample_free( ctx ); }
+  inline ResampleContext *resample_alloc() { return avresample_alloc_context(); }
+  inline int resample_init( ResampleContext *ctx ) { return avresample_open( ctx ); }
+ #endif
+#endif
+
+}
 
 class FeMediaImp;
 class FeAudioImp;
@@ -101,6 +148,8 @@ public:
 	static void set_current_decoder( const std::string & );
 
 protected:
+	static std::string g_decoder;
+
 	// overrides from base class
 	//
 	bool onGetData( Chunk &data );
@@ -115,12 +164,125 @@ private:
 	FeMediaImp *m_imp;
 	FeAudioImp *m_audio;
 	FeVideoImp *m_video;
-	static std::string g_decoder;
 
 	FeMedia( const FeMedia & );
 	FeMedia &operator=( const FeMedia & );
 	static void init_av();
 	float m_aspect_ratio;
+};
+
+//
+// Container for our general implementation
+//
+class FeMediaImp
+{
+public:
+	FeMediaImp( FeMedia::Type t );
+	void close();
+
+	FeMedia::Type m_type;
+	AVFormatContext *m_format_ctx;
+	AVIOContext *m_io_ctx;
+	sf::Mutex m_read_mutex;
+	bool m_read_eof;
+};
+
+//
+// Base class for our implementation of the audio and video components
+//
+class FeBaseStream
+{
+private:
+	//
+	// Queue containing the next packet to process for this stream
+	//
+	std::queue <AVPacket *> m_packetq;
+	sf::Mutex m_packetq_mutex;
+
+public:
+	virtual ~FeBaseStream();
+
+	bool at_end;					// set when at the end of our input
+	bool far_behind;
+	AVCodecContext *codec_ctx;
+	AVCodec *codec;
+	int stream_id;
+
+	FeBaseStream();
+	virtual void stop();
+	AVPacket *pop_packet();
+	void push_packet( AVPacket *pkt );
+	void clear_packet_queue();
+
+	// Utility functions to free AV stuff...
+	//
+	static void free_packet( AVPacket *pkt );
+	static void free_frame( AVFrame *frame );
+};
+
+//
+// Container for our implementation of the audio component
+//
+class FeAudioImp : public FeBaseStream
+{
+public:
+#ifdef DO_RESAMPLE
+	ResampleContext *resample_ctx;
+#endif
+	sf::Int16 *buffer;
+	sf::Mutex buffer_mutex;
+
+	FeAudioImp();
+	~FeAudioImp();
+};
+
+//
+// Container for our implementation of the video component
+//
+class FeVideoImp : public FeBaseStream
+{
+private:
+	//
+	// Video decoding and colour conversion runs on a dedicated thread.
+	// Loading the result into an sf::Texture and displaying it is done
+	// on the main thread.
+	//
+	sf::Thread m_video_thread;
+	FeMedia *m_parent;
+	sf::Uint8 *rgba_buffer[4];
+	int rgba_linesize[4];
+
+#if FE_HWACCEL
+	AVPixelFormat hwaccel_output_format;
+	bool hw_retrieve_data( AVFrame *f );
+#endif
+
+public:
+	bool run_video_thread;
+	sf::Time time_base;
+	sf::Time max_sleep;
+	sf::Clock video_timer;
+	sf::Texture *display_texture;
+	SwsContext *sws_ctx;
+	int sws_flags;
+	int disptex_width;
+	int disptex_height;
+
+	//
+	// The video thread sets display_frame when the next image frame is decoded.
+	// The main thread then copies the image into the corresponding sf::Texture.
+	//
+	sf::Mutex image_swap_mutex;
+	sf::Uint8 *display_frame;
+
+	FeVideoImp( FeMedia *parent );
+	~FeVideoImp();
+
+	void play();
+	void stop();
+
+	void preload();
+	void video_thread();
 };
 
 #endif
